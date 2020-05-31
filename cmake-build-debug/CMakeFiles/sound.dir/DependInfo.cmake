83633efa430a3d5fc59d5124ddfddbef157e6977
lukashov2002 <Jujik_8@mail.ru>
2020-05-31 16:53:12 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/sound.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/lukas/CLionProjects/sound/common.cpp" "C:/Users/lukas/CLionProjects/sound/cmake-build-debug/CMakeFiles/sound.dir/common.cpp.obj"
+  "C:/Users/lukas/CLionProjects/sound/common_platform.cpp" "C:/Users/lukas/CLionProjects/sound/cmake-build-debug/CMakeFiles/sound.dir/common_platform.cpp.obj"
+  "C:/Users/lukas/CLionProjects/sound/main.cpp" "C:/Users/lukas/CLionProjects/sound/cmake-build-debug/CMakeFiles/sound.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../FMOD/inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/sound.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sound.dir/common.cpp.obj"
+  "CMakeFiles/sound.dir/common_platform.cpp.obj"
+  "CMakeFiles/sound.dir/main.cpp.obj"
+  "sound.exe"
+  "sound.exe.manifest"
+  "sound.lib"
+  "sound.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sound.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
